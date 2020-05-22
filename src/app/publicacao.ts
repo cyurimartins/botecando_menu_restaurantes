41b69294b935282cf54d9f20963efd6f0cd86475
Yuri Martins <cyuri.martins@gmail.com>
2020-05-22 02:53:40 -0300

    a

---
+++ src/app/publicacao.ts
@@ -0,0 +1,10 @@
+export class Publicacao {
+    id?:string;
+    titulo?:string;
+    subtitulo?:string;
+    descricao?:string;
+    categoria?:string;
+    subcategoria?:string;
+     
+	       
+}
